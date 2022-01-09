a70b2890249b5359b725136078afa376cacb8a22
rai-roshan <kirito22037@gmail.com>
2022-01-10 00:32:17 +0530

    remove the full menu animation when page first loads

--- components/NavBtn.js
+++ components/NavBtn.js
@@ -1,29 +1,33 @@
 import React, { useState } from 'react';
 
-const FullScreenMenu = ({display, handleClick}) => (
-    <div className={`full-menu ${ display ? "animate-open-full-nav" : "animate-close-full-nav"}` }>
+const FullScreenMenu = ({display, closeMenu}) => (
+    <div className={`full-menu ${ display != null ? display ? "animate-open-full-nav" : "animate-close-full-nav" : "" }` }>
         <ul>
-            <li onClick={ handleClick } className={"menu-item"}><a href="#home">Home</a></li>
-            <li onClick={ handleClick } className={"menu-item"}><a href="#education">Education</a></li>
-            <li onClick={ handleClick } className={"menu-item"}><a href="#skills">Skills</a></li>
-            <li onClick={ handleClick } className={"menu-item"}><a href="#experience">Experience</a></li>
-            <li onClick={ handleClick } className={"menu-item"}><a href="#projects">Projects</a></li>
-            <li onClick={ handleClick } className={"menu-item"}><a href="#contact">Contact</a></li>
+            <li onClick={ closeMenu } className={"menu-item"}><a href="#home">Home</a></li>
+            <li onClick={ closeMenu } className={"menu-item"}><a href="#education">Education</a></li>
+            <li onClick={ closeMenu } className={"menu-item"}><a href="#skills">Skills</a></li>
+            <li onClick={ closeMenu } className={"menu-item"}><a href="#experience">Experience</a></li>
+            <li onClick={ closeMenu } className={"menu-item"}><a href="#projects">Projects</a></li>
+            <li onClick={ closeMenu } className={"menu-item"}><a href="#contact">Contact</a></li>
         </ul>
     </div>
-)
+);
+
 export default function NavBtn() {
-    const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(null);
 
-    const handleClick=()=>{
-        setOpen( !open );
-    };
+    const handleMenu=()=>{
+        if( open === null )
+        setOpen(true);
 
+        else
+            setOpen(!open);
+    }
     return (<div className="fixed bottom-7 right-7 flex flex-col items-end">
 
-        <FullScreenMenu display={open} handleClick={()=>{ handleClick()} } />
+        <FullScreenMenu display={open} closeMenu={()=>{ handleMenu() } } />
 
-        <div className="z-50 cursor-pointer bg-gray-50 p-3 rounded-full shadow-2xl" onClick={ handleClick } >
+        <div className="z-50 cursor-pointer bg-gray-50 p-3 rounded-full shadow-2xl" onClick={ handleMenu } >
             { open ?  <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24"
                           stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"/>
